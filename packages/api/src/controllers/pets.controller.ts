d42b876c89d61d0f21288f29f95c227775b3f093
Paul Souche <paul.souche@gmail.com>
2019-11-03 17:47:37 +0100

    :ambulance: fix ids symbol issue

--- packages/api/src/controllers/pets.controller.ts
+++ packages/api/src/controllers/pets.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiUseTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiUseTags } from '@nestjs/swagger';
 import PetCreateDto from '../models/pets/pet-create.dto';
 import petId from '../models/pets/pet-id';
 import PetUpdateDto from '../models/pets/pet-update.dto';
@@ -15,6 +15,11 @@ export default class PetsController {
   @Get()
   @ApiOkResponse({ description: 'Returns a list of pets', type: [PetDto] })
   @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiParam({
+    description: 'user id',
+    name: 'userId',
+    type: String,
+  })
   getPets(@Param('userId') uId: userId): PetDto[] {
     return this.petsService.getPetsForUsers(uId);
   }
@@ -22,6 +27,16 @@ export default class PetsController {
   @Get(':id')
   @ApiOkResponse({ description: 'Returns a pet', type: PetDto })
   @ApiNotFoundResponse({ description: 'User or Pet not found' })
+  @ApiParam({
+    description: 'user id',
+    name: 'userId',
+    type: String,
+  })
+  @ApiParam({
+    description: 'pet id',
+    name: 'id',
+    type: String,
+  })
   getPet(@Param('userId') uId: userId, @Param('id') id: petId): PetDto {
     return this.petsService.getPet(uId, id);
   }
@@ -30,6 +45,11 @@ export default class PetsController {
   @ApiCreatedResponse({ description: 'Creates a pet', type: PetDto })
   @ApiBadRequestResponse({ description: 'Bad request' })
   @ApiNotFoundResponse({ description: 'User or Pet not found' })
+  @ApiParam({
+    description: 'user id',
+    name: 'userId',
+    type: String,
+  })
   createUser(@Param('userId') uId: userId, @Body() pet: PetCreateDto): PetDto {
     return this.petsService.addPet(uId, pet);
   }
@@ -38,6 +58,16 @@ export default class PetsController {
   @ApiOkResponse({ description: 'Updates a pet', type: PetDto })
   @ApiBadRequestResponse({ description: 'Bad request' })
   @ApiNotFoundResponse({ description: 'User or Pet not found' })
+  @ApiParam({
+    description: 'user id',
+    name: 'userId',
+    type: String,
+  })
+  @ApiParam({
+    description: 'pet id',
+    name: 'id',
+    type: String,
+  })
   updateUser(@Param('userId') uId: userId, @Param('id') id: petId, @Body() pet: PetUpdateDto): PetDto {
     return this.petsService.updatePet(uId, id, pet);
   }
@@ -45,6 +75,16 @@ export default class PetsController {
   @Delete(':id')
   @ApiOkResponse({ description: 'Deletes a pet', type: PetDto })
   @ApiNotFoundResponse({ description: 'User or Pet not found' })
+  @ApiParam({
+    description: 'user id',
+    name: 'userId',
+    type: String,
+  })
+  @ApiParam({
+    description: 'pet id',
+    name: 'id',
+    type: String,
+  })
   deleteUser(@Param('userId') uId: userId, @Param('id') id: petId): PetDto {
     return this.petsService.removePet(uId, id);
   }
